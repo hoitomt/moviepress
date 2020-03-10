b937e5bbcc7e9cd5878badbf924349181d3c83d4
Mike Hoitomt <mhoitomt@lgscout.com>
2020-03-09 22:01:13 -0500

    add controllers

--- .progress.sh
+++ .progress.sh
@@ -1,1 +1,1 @@
-echo You are on step 1
+echo You are on step 5: https://dashboards.online.bootcampspot.com/modules/node-js/5
--- server.js
+++ server.js
@@ -2,9 +2,8 @@ console.log("Starting Server...");
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+// Looks for index.js in the controllers folder (similar to __init__ with Python)
+app.use(require('./controllers'))
 
 const port = process.env.PORT || 3001;
 
