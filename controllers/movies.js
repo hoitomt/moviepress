531c7c811a103d213b4234d863a03174a2219524
Mike Hoitomt <mhoitomt@lgscout.com>
2020-03-30 15:47:25 -0500

    Step 10 - build the index.html

--- controllers/movies.js
+++ controllers/movies.js
@@ -6,13 +6,13 @@ router.get('/', (req, res) => {
   const popularMoviesURL = `http://api.themoviedb.org/3/movie/popular?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1`;
   axios.get(popularMoviesURL)
   .then((api_res) => {
-    console.log(api_res);
     res.render('application', {
+      locals: {
+        movies: api_res.data
+      },
       partials: {
         yield: 'views/movies/index.html'
-      }, locals: {
-
-      }
+      },
     });
   })
   .catch((err) => {
